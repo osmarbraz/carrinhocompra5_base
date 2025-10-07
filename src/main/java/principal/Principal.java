00ee01333f33742e7c4a9662fbc9c31ce2f5cde9
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2025-10-07 16:03:13 -0300

    fix: atualização testes

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -9,8 +9,7 @@ import javax.swing.JOptionPane;
 public class Principal {
 
     public static void main(String[] args) {
-
-        String opcao1 = "";
+        
         //Produtos pré-cadastrados
         Produto produto1 = new Produto("Livro", 35.0);
         Produto produto2 = new Produto("Caneta", 5.0);
@@ -22,52 +21,66 @@ public class Principal {
         //Servico de pagamento do carrinho
         ServicoPagamento pagar = new ServicoPagamento();
 
+        String opcao1 = "";
+        //Menu 1
         while (!opcao1.equals("9")) {
             opcao1 = JOptionPane.showInputDialog("1 - Adicionar produto ao carrinho \n2 - Quantidade produtos no carrinho \n3 - Listar nomes produtos \n4 - Saldo carrinho \n5 - Pagar \n9 - Sair");
             switch (Integer.parseInt(opcao1)) {
-                //Adição de produto ao carrinho
-                case 1:
+                //Menu para adição de produto ao carrinho
+                case 1: {
+                    //Menu 2
                     String opcao2 = "";
                     while (!opcao2.equals("9")) {
                         opcao2 = JOptionPane.showInputDialog("1 - Adicionar: Livro (35.0) \n2 - Adicionar: Caneta (5.0) \n3 - Adicionar: Notebook (2000.0) \n9 - Voltar");
                         switch (Integer.parseInt(opcao2)) {
-                            case 1:
+                            case 1: {
                                 carrinho.adicionar(produto1);
                                 System.out.println("Produto 1 adicionado ao carrinho");
                                 break;
-                            case 2:
+                            }
+                            case 2: {
                                 carrinho.adicionar(produto2);
                                 System.out.println("Produto 2 adicionado ao carrinho");
                                 break;
-                            case 3:
+                            }
+                            case 3: {
                                 carrinho.adicionar(produto3);
                                 System.out.println("Produto 3 adicionado ao carrinho");
                                 break;
-                            case 9:
+                            }
+                            case 9: {
                                 System.out.println("Voltando ao menu principal");
                                 break;
-                            default:
+                            }
+                            default: {
                                 JOptionPane.showMessageDialog(null, "Alternativa inválida!");
                                 break;
+                            }
                         }
                     }
                     break;
-                case 2:
+                }
+                //Mostrar a quantidade de produtos no carrinho
+                case 2: {
                     System.out.println("Quantidade de produtos no carrinho:" + carrinho.getQuantidadeProdutos());
                     break;
-                case 3:
+                }
+                //Mostrar os produtos do carrinho
+                case 3: {
                     System.out.println("Produtos do carrinho:");
                     List<String> nomes = carrinho.getNomeProdutos();
                     for (int i = 0; i < nomes.size(); i++) {
                         System.out.println(nomes.get(i));
                     }
                     break;
+                }
                 //Mostrar o total do carrinho
-                case 4:
+                case 4: {
                     JOptionPane.showMessageDialog(null, "Total carrinho: " + carrinho.getTotal());
                     break;
+                }
                 //Pagar o carrinho
-                case 5:
+                case 5: {
                     double valor = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor para pagar o carrinho:"));
                     pagar.setSaldo(valor);
                     if (pagar.pagar(carrinho)) {
@@ -76,12 +89,16 @@ public class Principal {
                         JOptionPane.showMessageDialog(null, "Atenção: saldo insuficiente para pagar o carrinho");
                     }
                     break;
-                case 9:
+                }
+                //Sair do sistema
+                case 9: {
                     System.exit(0);
                     break;
-                default:
+                }
+                default: {
                     JOptionPane.showMessageDialog(null, "Alternativa inválida!");
                     break;
+                }
             }
         }
     }
